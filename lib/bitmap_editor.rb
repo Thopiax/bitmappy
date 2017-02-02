2c6abfd8c5eca4dd61b9f2138ce3281624fa083f
Rafael Toletti Ballestiero <rb2215@ic.ac.uk>
2017-02-02 13:30:36 +0000

    Added framework for other commands

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -19,9 +19,23 @@ class BitmapEditor
           check_params input, 0
           clear_image
         when 'L'
+          check_params input, 3
+          c = input.pop
+          x, y = *(input.map &:to_i)
+          set_element_color x, y, c
         when 'V'
+          check_params input, 4
+          c = input.pop
+          x, v1, v2 = *(input.map &:to_i)
+          set_color_column x, v1, v2, c
         when 'H'
+          check_params input, 4
+          c = input.pop
+          h1, h2, y = *(input.map &:to_i)
+          set_color_row h1, h2, y, c
         when 'S'
+          check_params input, 0
+          show_image
         when '?'
           show_help
         when 'X'
@@ -51,6 +65,30 @@ class BitmapEditor
       @image = Array.new(@n) { Array.new(@m, WHITE_COLOR) }
     end
 
+    def set_element_color(x, y, c)
+      @image[get_index(y)][get_index(x)] = c
+    end
+
+    def set_color_column(x, v1, v2, c)
+      v1..v2.each do |y|
+        set_element_color(x, y, c)
+      end
+    end
+
+    def set_color_row(h1, h2, y, c)
+      h1..h2.each do |x|
+        set_element_color(x, y, c)
+      end
+    end
+
+    def show_image
+      if @image.empty?
+        puts 'you need to create an image first!'
+      else
+        @image.each { |row| puts row.join('') }
+      end
+    end
+
     def exit_console
       puts 'goodbye!'
       @running = false
@@ -66,11 +104,12 @@ H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 
 S - Show the contents of the current image
 X - Terminate the session"
     end
+end
 
-    def check_params(input, n)
-       raise_arg_error "wrong number of arguments" unless input.length == n
-    end
+# ERROR HANDLING
 
+def check_params(input, n)
+   raise_arg_error "wrong number of arguments" unless input.length == n
 end
 
 def raise_arg_error(msg)
