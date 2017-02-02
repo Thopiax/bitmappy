43e456561baf8f56d2cf5eee4b0109b97e1115d1
Rafael Toletti Ballestiero <rb2215@ic.ac.uk>
2017-02-02 12:43:27 +0000

    implemented I and C cases and parameter check

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,30 +1,49 @@
 class BitmapEditor
+  WHITE_COLOR = 'O'
 
   def run
+    @image = []
     @running = true
     puts 'type ? for help'
     while @running
       print '> '
       input = gets.chomp.split
       cmd = input.shift
-      case cmd
-      when 'I'
-      when 'C'
-      when 'L'
-      when 'V'
-      when 'H'
-      when 'S'
-      when '?'
-        show_help
-      when 'X'
-        exit_console
-      else
-        puts 'unrecognised command :('
+      begin
+        case cmd
+        when 'I'
+          check_params input, 2
+          init_image *input
+        when 'C'
+          check_params input, 0
+          clear_image
+        when 'L'
+        when 'V'
+        when 'H'
+        when 'S'
+        when '?'
+          show_help
+        when 'X'
+          exit_console
+        else
+          puts 'unrecognised command :('
+        end
+      rescue ArgumentError => e
+        "invalid command: #{e}"
       end
     end
   end
 
   private
+
+    def init_image(n, m)
+
+    end
+
+    def clear_image
+
+    end
+
     def exit_console
       puts 'goodbye!'
       @running = false
@@ -39,5 +58,9 @@ V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 an
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
 S - Show the contents of the current image
 X - Terminate the session"
+    end
+
+    def check_params(input, n)
+      raise ArgumentError, "wrong number of arguments" unless input.length == n
     end
 end
