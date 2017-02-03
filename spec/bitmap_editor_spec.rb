035544e91967c88e9d5d3bae2b14da1b36924c72
Rafael Toletti Ballestiero <rb2215@ic.ac.uk>
2017-02-03 13:03:32 +0000

    Improved test code quality and created tests for editor

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,20 +1,23 @@
-require "bitmap_editor"
+require 'bitmap_editor.rb'
 
 describe BitmapEditor do
-  before do
-    _, @writer = IO.pipe
-    @bit_edit = BitmapEditor.new
-  end
 
-  describe "initialize" do
-    context "given 2 positive parameters" do
-      it "creates a new image and stores the coordinates" do
-        @writer.puts "I 2 3"
-        @bit_edit.run
-        expect(@bit_edit.m).to eq(3)
-        expect(@bit_edit.n).to eq(2)
-        expect(@bit_edit.image).to eq([['O','O'],['O','O'],['O','O']])
-      end
+  subject { BitmapEditor.new }
+
+  describe "#analyse_command" do
+    it "simple show" do
+      subject.analyse_command("I 5 6")
+      expect { subject.analyse_command("S") }.to output("OOOOO
+OOOOO
+OOOOO
+OOOOO
+OOOOO
+OOOOO
+").to_stdout
+    end
+
+    it "not initialized" do
+      expect { subject.analyse_command("S") }.to output("invalid command: you need to create an image first!\n").to_stdout
     end
   end
 
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -5,16 +5,18 @@ WIDTH  = 5
 HEIGHT = 6
 
 describe Bitmap do
+
+  subject { Bitmap.new }
+
   before :each do
-    @bitmap = Bitmap.new
-    @bitmap.create_image(WIDTH, HEIGHT)
+    subject.create_image(WIDTH, HEIGHT)
   end
 
   describe '#initialize' do
     context 'with good arguments' do
-      it { expect(@bitmap.width).to eq WIDTH }
-      it { expect(@bitmap.height).to eq HEIGHT }
-      it { expect(@bitmap.image).not_to be_empty }
+      it { expect(subject.width).to eq WIDTH }
+      it { expect(subject.height).to eq HEIGHT }
+      it { expect(subject.image).not_to be_empty }
     end
 
     context 'with bad arguments' do
@@ -25,29 +27,29 @@ describe Bitmap do
   describe '#set_pixel' do
     context 'with good arguments' do
       it do
-        expect(@bitmap.get_pixel(1, 1)).to eq 'O'
-        @bitmap.set_pixel(1, 1, 'C')
-        expect(@bitmap.get_pixel(1, 1)).to eq 'C'
+        expect(subject.get_pixel(1, 1)).to eq 'O'
+        subject.set_pixel(1, 1, 'C')
+        expect(subject.get_pixel(1, 1)).to eq 'C'
       end
     end
 
     context 'with out of range indeces' do
-      it { expect { @bitmap.set_pixel(1, 260, 'C') }.to raise_error ArgumentError }
-      it { expect { @bitmap.set_pixel(-1, 5, 'C') }.to raise_error ArgumentError }
+      it { expect { subject.set_pixel(1, 260, 'C') }.to raise_error ArgumentError }
+      it { expect { subject.set_pixel(-1, 5, 'C') }.to raise_error ArgumentError }
     end
 
     context 'with invalid color' do
-      it { expect { @bitmap.set_pixel(1, 1, 'c') }.to raise_error ArgumentError }
-      it { expect { @bitmap.set_pixel(1, 1, 5) }.to raise_error ArgumentError }
+      it { expect { subject.set_pixel(1, 1, 'c') }.to raise_error ArgumentError }
+      it { expect { subject.set_pixel(1, 1, 5) }.to raise_error ArgumentError }
     end
   end
 
-  describe '#clear' do
+  describe '#clear_image' do
     context 'clears a colored board' do
       it do
-        @bitmap.set_pixel(1, 1, 'C')
-        @bitmap.clear_image
-        expect(@bitmap.image).to eq Bitmap.new.create_image(WIDTH, HEIGHT).image
+        subject.set_pixel(1, 1, 'C')
+        subject.clear_image
+        expect(subject.image).to eq Bitmap.new.create_image(WIDTH, HEIGHT)
       end
     end
   end
@@ -55,23 +57,23 @@ describe Bitmap do
   describe '#set_column' do
     context 'with good arguments' do
       it do
-        @bitmap.set_column(1, 1, 5, 'C')
+        subject.set_column(1, 1, 5, 'C')
         (1..5).each do |y|
-          expect(@bitmap.get_pixel(1, y)).to eq('C')
+          expect(subject.get_pixel(1, y)).to eq('C')
         end
       end
     end
 
     context 'with bad arguments' do
-      it { expect { @bitmap.set_column(-1, 5, 1, 'C') }.to raise_error ArgumentError }
-      it { expect { @bitmap.set_column(1, 5, -1, 'C') }.to raise_error ArgumentError }
+      it { expect { subject.set_column(-1, 5, 1, 'C') }.to raise_error ArgumentError }
+      it { expect { subject.set_column(1, 5, -1, 'C') }.to raise_error ArgumentError }
     end
 
     context 'with order reversed' do
       it do
-        @bitmap.set_column(1, 5, 1, 'C')
+        subject.set_column(1, 5, 1, 'C')
         (1..5).each do |y|
-          expect(@bitmap.get_pixel(1, y)).to eq('C')
+          expect(subject.get_pixel(1, y)).to eq('C')
         end
       end
     end
@@ -80,34 +82,41 @@ describe Bitmap do
   describe '#set_row' do
     context 'with good arguments' do
       it do
-        @bitmap.set_row(1, 5, 1, 'C')
+        subject.set_row(1, 5, 1, 'C')
         (1..5).each do |x|
-          expect(@bitmap.get_pixel(x, 1)).to eq('C')
+          expect(subject.get_pixel(x, 1)).to eq('C')
         end
       end
     end
 
     context 'with out of bound indeces' do
-      it { expect { @bitmap.set_row(-1, 5, 1, 'C') }.to raise_error ArgumentError }
-      it { expect { @bitmap.set_row(1, 5, -1, 'C') }.to raise_error ArgumentError }
+      it { expect { subject.set_row(-1, 5, 1, 'C') }.to raise_error ArgumentError }
+      it { expect { subject.set_row(1, 5, -1, 'C') }.to raise_error ArgumentError }
     end
 
     context 'with order reversed' do
       it do
-        @bitmap.set_row(5, 1, 1, 'C')
+        subject.set_row(5, 1, 1, 'C')
         (1..5).each do |x|
-          expect(@bitmap.get_pixel(x, 1)).to eq('C')
+          expect(subject.get_pixel(x, 1)).to eq('C')
         end
       end
     end
   end
 
   describe '#show' do
-    it do
-      @bitmap.set_row(5, 1, 1, 'C')
-      @bitmap.set_column(1, 1, 5, 'W')
-      @bitmap.set_column(3, 3, 6, 'Z')
-      expect(@bitmap.show).to eq([%w(W C C C C),
+    it "show empty matrix" do
+      expect(subject.show).to eq([%w(O O O O O),
+                                  %w(O O O O O), %w(O O O O O),
+                                  %w(O O O O O), %w(O O O O O),
+                                  %w(O O O O O)])
+    end
+
+    it "show complex matrix" do
+      subject.set_row(5, 1, 1, 'C')
+      subject.set_column(1, 1, 5, 'W')
+      subject.set_column(3, 3, 6, 'Z')
+      expect(subject.show).to eq([%w(W C C C C),
                                   %w(W O O O O), %w(W O Z O O),
                                   %w(W O Z O O), %w(W O Z O O),
                                   %w(O O Z O O)])
